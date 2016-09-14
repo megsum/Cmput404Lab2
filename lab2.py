c4e32ddaba203d32a3d744d9b6a2b1281495f727
megsum <msumner@ualberta.ca>
2016-09-14 15:56:15 -0600

    Final edits

--- lab2.py
+++ lab2.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 # Allocate a new socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,38 +9,41 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('0.0.0.0', 8000))
 server.listen(1)
 
-print "Waiting for connections..."
-client, address = server.accept()
-print "Connected!"
-print address
-
-#client is going to be curl, web browser, etc.
-outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-outgoing.connect(("www.google.com", 80))
-outgoing.setblocking(0)
-client.setblocking(0)
 while True:
-    try:
-        part = client.recv(1024)
-    except socket.error, exception:
-        if exception.errno == 11:
-	    part = None
-	else:
-	    raise
-    if (part is not None and len(part) ==0):
-       exit(0)
-    if (part):
-       print " < " + part
-       outgoing.sendall(part)
-    try:
-        part = outgoing.recv(1024)
-    except socket.error, exception:
-        if exception.errno == 11:
-	    part = None
-	else:
-	    raise
-    if (part is not None and len(part) ==0):
-       exit(0)
-    if (part):
-       print " > " + part
-       client.sendall(part)
+    print "Waiting for connections..."
+    client, address = server.accept()
+    print "Connected!"
+    print address
+
+    pid = os.fork()
+    if (pid ==0): # we are in the child process
+        #client is going to be curl, web browser, etc.
+        outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        outgoing.connect(("www.google.com", 80))
+        outgoing.setblocking(0)
+        client.setblocking(0)
+        while True:
+            try:
+                part = client.recv(1024)
+            except socket.error, exception:
+                if exception.errno == 11:
+	            part = None
+	        else:
+	            raise
+            if (part is not None and len(part) ==0):
+               exit(0)
+            if (part):
+               print " < " + part
+               outgoing.sendall(part)
+            try:
+                part = outgoing.recv(1024)
+            except socket.error, exception:
+                if exception.errno == 11:
+                    part = None
+   	        else:
+	            raise
+            if (part is not None and len(part) ==0):
+                exit(0)
+            if (part):
+                print " > " + part
+                client.sendall(part)
