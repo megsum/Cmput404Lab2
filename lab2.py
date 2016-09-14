90e88bae7e08ef6ba643176bb26f34b6a8129492
megsum <msumner@ualberta.ca>
2016-09-14 14:37:39 -0600

    server socket

--- lab2.py
+++ lab2.py
@@ -1,21 +1,13 @@
 import socket
 
 # Allocate a new socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Connect to google.ca
-client.bind(('0.0.0.0', 8000))
+server.bind(('0.0.0.0', 8000))
+server.listen(1)
 
-http = 'GET / HTTP/1.0\r\n\r\n'
-
-client.sendall(http)
-
-msg = ""
-while True:
-    part = client.recv(1024)
-    if part:
-     	msg += part
-    else:
-	break
-
-print msg    
+print "Waiting for connections..."
+client, address = server.accept()
+print "Connected!"
+print address
