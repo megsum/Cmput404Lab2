dfb1d036c82cadd19fcd59fe52ea826d09a3173f
megsum <msumner@ualberta.ca>
2016-09-14 14:52:07 -0600

    proxy code

--- lab2.py
+++ lab2.py
@@ -3,7 +3,7 @@ import socket
 # Allocate a new socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Connect to google.ca
+#Listen on port 8000
 server.bind(('0.0.0.0', 8000))
 server.listen(1)
 
@@ -11,3 +11,16 @@ print "Waiting for connections..."
 client, address = server.accept()
 print "Connected!"
 print address
+
+#client is going to be curl, web browser, etc.
+outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+outgoing.connect(("www.google.ca", 80))
+while True:
+    part = client.recv(1024)
+    print " < " + part
+    if (part):
+       outgoing.sendall(part)
+    part = outgoing.recv(1024)
+    print " > " + part
+    if (part):
+       client.sendall(part)
